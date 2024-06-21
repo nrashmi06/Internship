5722ad9afbb75fbabccd89ea3c9a03c4d9facddd
nrashmi06 <rashminattoji24@gmail.com>
2024-06-21 12:01:49 +0530

    TASK 1

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,15 +1,6 @@
 import './App.css';
 import React from 'react';
-import Intro from './Intro';
 import Car from './Car';
-import StateObj from './StateObj';
-import Update from './Update';
-import Football from './Football';
-import Garage from './Garage';
-import FavoriteColor from './FavoriteColor';
-import Counter from './Counter';
-import Todos from './ToDo';
-
 
 function App() {
   return (
