b56fd76e84c8f13aebb4ef3ad49eb20b1ef227b0
nrashmi06 <rashminattoji24@gmail.com>
2024-08-08 21:56:38 +0530

    check

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,14 @@ app.use('/api/users', profileRouter);
 app.use('/api/users', commentRoutes);
 app.use('/api/auth', refreshRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
