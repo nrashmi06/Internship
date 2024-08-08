9ce6185d5c8e710b2e9e9306a4a40a9b3802aec2
nrashmi06 <rashminattoji24@gmail.com>
2024-08-08 21:19:40 +0530

    check

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use('/api/users', profileRouter);
 app.use('/api/users', commentRoutes);
 app.use('/api/auth', refreshRoutes);
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
